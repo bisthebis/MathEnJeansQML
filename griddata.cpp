5a71116e7143fd128e5bc845866c0adb84b3d479
bisthebis <bism25@hotmail.com>
2016-08-13 12:19:52 +0200

    Computing algorithm

--- griddata.cpp
+++ griddata.cpp
@@ -7,5 +7,26 @@ GridData::GridData(QObject *parent) : QObject(parent)
 
 void GridData::computeResult()
 {
-    //PLACEHOLDER
+    bool isCorrect = true; //Unless a false value is found
+    for (int y = 0; y < height; ++y)
+    {
+        for (int x = 0; x < width; ++x)
+        {
+            bool newValue = valueAt(x, y)
+                          ^ safeValueAt(x - 1, y)
+                          ^ safeValueAt(x + 1, y)
+                          ^ safeValueAt(x, y - 1)
+                          ^ safeValueAt(x, y + 1);
+
+            isCorrect &= newValue;
+        }
+    }
+
+    emit resultChanged();
+    if (isCorrect != _correct)
+    {
+        _correct = isCorrect;
+        emit correctChanged();
+    }
+
 }
